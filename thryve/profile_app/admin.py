f963be20330459155437a69074336ded6d7e62a1
Justine Vilocura <justine.vilocura@cit.edu>
2025-10-10 05:27:23 +0800

    feature(profile):fixing error for admin in profile_app

--- thryve/profile_app/admin.py
+++ thryve/profile_app/admin.py
@@ -1,16 +1,10 @@
 from django.contrib import admin
-from .models import BusinessProfile, ProfileCustomization  # Correct import from profile_app.models
+from .models import BusinessProfile, ProfileCustomization
 
-# Register BusinessProfile with admin
 @admin.register(BusinessProfile)
 class BusinessProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'company_name', 'industry', 'website_url')  # Fields to display in list view
-    search_fields = ('company_name', 'industry')  # Fields that can be searched in admin
-    list_filter = ('industry',)  # Add a filter for the industry field
+    list_display = ('user', 'company_name', 'industry', 'website_url')
 
-# Register ProfileCustomization with admin
 @admin.register(ProfileCustomization)
 class ProfileCustomizationAdmin(admin.ModelAdmin):
-    list_display = ('user', 'display_name', 'tagline')  # Fields to display in list view
-    search_fields = ('display_name', 'tagline')  # Fields that can be searched in admin
-    list_filter = ('tagline',)  # Add a filter for the tagline field
+    list_display = ('user', 'display_name', 'tagline')
