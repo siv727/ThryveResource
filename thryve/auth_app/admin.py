317cbdcf230d474425fcb6deb663f28ca8e8c77c
Justine Vilocura <justine.vilocura@cit.edu>
2025-10-10 04:07:51 +0800

    feature(profile):added code in admin.py

--- thryve/auth_app/admin.py
+++ thryve/auth_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import BusinessProfile, ProfileCustomization
 
-# Register your models here.
+@admin.register(BusinessProfile)
+class BusinessProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'company_name', 'industry', 'website_url')
+
+@admin.register(ProfileCustomization)
+class ProfileCustomizationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'display_name', 'tagline')
