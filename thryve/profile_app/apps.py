6cbc26b4d4d805a0e6d5a87848ddc90fb37263f7
Justine Vilocura <justine.vilocura@cit.edu>
2025-10-10 03:49:10 +0800

    feature(profile): changesin app.py

--- thryve/profile_app/apps.py
+++ thryve/profile_app/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class ProfileAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'profile_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "profile_app"
+
+    def ready(self):
+        from . import signals  # noqa
