f4d342f11873975139c92200ece57b8ff7de9584
Justine Vilocura <justine.vilocura@cit.edu>
2025-10-10 05:29:03 +0800

    feature(profile): No need to register BusinessProfile and ProfileCustomization here, it should be in profile_app/admin.py

--- thryve/auth_app/admin.py
+++ thryve/auth_app/admin.py
@@ -1,14 +1,5 @@
 from django.contrib import admin
-from profile_app.models import BusinessProfile, ProfileCustomization  # Correct import from profile_app
+from profile_app.models import BusinessProfile, ProfileCustomization  # Import from profile_app
 
-@admin.register(BusinessProfile)
-class BusinessProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'company_name', 'industry', 'website_url')  # Ensure fields exist in the model
-    search_fields = ('company_name', 'industry')
-    list_filter = ('industry',)
-
-@admin.register(ProfileCustomization)
-class ProfileCustomizationAdmin(admin.ModelAdmin):
-    list_display = ('user', 'display_name', 'tagline')  # Ensure fields exist in the model
-    search_fields = ('display_name', 'tagline')
-    list_filter = ('tagline',)
+# Remove the registration code from auth_app/admin.py
+# No need to register BusinessProfile and ProfileCustomization here, it should be in profile_app/admin.py
