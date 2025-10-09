74127ec745308e85b6546d0c82c3210351b6d3fa
Justine Vilocura <justine.vilocura@cit.edu>
2025-10-10 04:13:24 +0800

    feature(profile): settings added media_url and media_root

--- thryve/thryve/settings.py
+++ thryve/thryve/settings.py
@@ -21,6 +21,11 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# MEDIA_URL (URL for serving files)
+MEDIA_URL = '/media/'
+
+# MEDIA_ROOT (where media files are stored on the server)
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
