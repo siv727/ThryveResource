93b7915c520e6f3edc33786c38224762f48699eb
Justine Vilocura <justine.vilocura@cit.edu>
2025-10-10 22:55:24 +0800

    fix(login): fixing the authentication

--- thryve/auth_app/authentication_backend.py
+++ thryve/auth_app/authentication_backend.py
@@ -1,10 +1,13 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
+
+# Get the custom user model dynamically
+User = get_user_model()
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, email=None, password=None, **kwargs):
         try:
-            # Attempt to get the user based on email
+            # Attempt to get the user based on email using the custom user model
             user = User.objects.get(email=email)
             # Check the user's password
             if user.check_password(password):
