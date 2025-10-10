8a455c59f477793ca41ffa283bb74af05ebb785e
Justine Vilocura <justine.vilocura@cit.edu>
2025-10-10 17:55:37 +0800

    feature(profile): made changes in signals profile_app:

--- thryve/profile_app/signals.py
+++ thryve/profile_app/signals.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import BusinessProfile, ProfileCustomization
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_profiles(sender, instance, created, **kwargs):
     if created:
         BusinessProfile.objects.create(user=instance)
