1f70a71031f7c5a359ce4a0a231c62de72c569a6
Justine Vilocura <justine.vilocura@cit.edu>
2025-10-10 18:19:51 +0800

    feature(profile): changes in the urls

--- thryve/thryve/urls.py
+++ thryve/thryve/urls.py
@@ -1,32 +1,20 @@
-"""
-URL configuration for thryve project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Import the views module from auth_app to use the home view
+from auth_app import views  # Add this import
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('auth_app.urls')),              # you already have home/login/register here
-    path('profile/', include('profile_app.urls')), 
-     # Add this line to handle the root (empty) URL:
-    path('', views.home, name='home'),  # Home page view (you can customize this)# ← add this
+    path('auth/', include('auth_app.urls')),  # Authentication URLs
+    path('profile/', include('profile_app.urls')),  # Profile URLs
+
+    # Home page route
+    path('', views.home, name='home'),  # Home page view (you can customize this)
 ]
 
-# Serve media in development (for avatars/logos)
+# Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
