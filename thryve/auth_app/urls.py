80e26c7be93323cdfab3610b83ffaff4a9ec49be
Justine Vilocura <justine.vilocura@cit.edu>
2025-10-10 16:26:22 +0800

    feature(login): added urls

--- thryve/auth_app/urls.py
+++ thryve/auth_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from auth_app import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.logout, name='logout'),
