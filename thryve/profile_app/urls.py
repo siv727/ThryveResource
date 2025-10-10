ac46e22a9eba1cb5c251868693993551311027b8
Justine Vilocura <justine.vilocura@cit.edu>
2025-10-10 17:01:18 +0800

    feature(profile): changes in urls

--- thryve/profile_app/urls.py
+++ thryve/profile_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('home/', views.home, name='home'),
     path('business/', views.business_profile_view, name='business_profile'),
     path('customize/', views.profile_customization_view, name='profile_customization'),
 ]
